5c3cdf2668f176c0546f8aa76aec1bf9108be685
Francois-Regis Pastol <redgy.p@gmail.com>
2024-11-16 03:05:34 +0100

    base MySQL créée/vérifiée,  models ok .env ok bdd.js ok @next : sequelize puis routes ...

---
+++ models/Document.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Document = sequelize.define('Document', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  dueDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+  },
+  recipient: { // Champ pour le destinataire
+    type: DataTypes.STRING,
+  },
+  createdBy: { // Champ pour l'ID de l'utilisateur qui a créé le document
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Users', // Nom du modèle d'utilisateur
+      key: 'id',
+    },
+  },
+}, {
+  timestamps: true, // Cela ajoute automatiquement les champs createdAt et updatedAt
+});
+
+module.exports = Document;
---
+++ models/Message.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+
+const Message = sequelize.define('Message', {
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    authorId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users', // Nom du modèle d'utilisateur
+        key: 'id',
+      },
+    },
+  }, {
+    timestamps: true, // Ajoute createdAt et updatedAt automatiquement
+  });
+  
+  module.exports = Message;
